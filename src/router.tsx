523ffe383e218147d22b8d1d35d55c5dcdf9314f
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-09-26 23:55:36 -0300

    correcciones menores

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import IndexPage from './views/IndexPage'
 import Layout from './layouts/Layout'
 
+const IndexPage = lazy(() => import('./views/IndexPage')) 
 const FavoritesPage = lazy(() => import('./views/FavoritesPage')) 
 
 export default function AppRouter() {
@@ -10,7 +10,11 @@ export default function AppRouter() {
     <BrowserRouter>
         <Routes>
             <Route element={<Layout />} >
-              <Route path='/' element={<IndexPage />} index />
+              <Route path='/' element={
+                <Suspense fallback="Loading...">
+                    <IndexPage />
+                </Suspense>
+              } index />
               <Route path='/favorites' element={
                 <Suspense fallback="Loading...">
                     <FavoritesPage />
